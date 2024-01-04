a64306cb3760baec05e061ff85a6006f5ca3fae6
choigukhee <cghee0718@naver.com>
2024-01-04 13:24:10 +0900

    백준 1158(요세푸스)

---
+++ CT (230104)/백준1158(요세푸스).py
@@ -0,0 +1,18 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+data = [i for i in range(1,n+1)]
+index = 0
+result = []
+
+for _ in range(n):
+  index += (k-1)
+  if index >= len(data):
+    index %= len(data)
+  result.append(str(data[index]))
+  data.pop(index)
+  
+print("<", end='')
+print(", ".join(result), end='')
+print(">")
+  
