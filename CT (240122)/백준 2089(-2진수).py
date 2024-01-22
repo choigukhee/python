7d4f9de373ea4f3767f9665a89a9b1d414166b41
choigukhee <cghee0718@naver.com>
2024-01-22 11:59:32 +0900

    백준 2089(-2진수)

---
+++ CT (240122)/백준 2089(-2진수).py
@@ -0,0 +1,24 @@
+import sys
+
+def divide(n, ans):
+  if n == 0:
+    ans.append(0)
+    return
+  if n == 1:
+    ans.append(1)
+    return
+  if n % 2 == 0:
+    ans.append(0)
+    divide(n // -2, ans)
+  elif n < 0:
+    ans.append(1)
+    divide((n // -2) + 1, ans)
+  else:
+    ans.append(1)
+    divide((n // -2) + 1, ans)
+    
+num = int(sys.stdin.readline().rstrip())
+ans = []
+divide(num, ans)
+ans.reverse()
+print(int(''.join(map(str,ans))))
